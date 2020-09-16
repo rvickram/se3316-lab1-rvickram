69d32e6ac540ae450548bf557941d7e1f8510172
Ryan Vickramasinghe <rvickram@uwo.ca>
2020-09-16 17:53:15 -0400

    Updated displaying of search results. (built-in alert was not long enough).

--- js/search.js
+++ js/search.js
@@ -1,27 +1,25 @@
 var pokemonList = []
 
-function load() {
-    pokemonList.push({number:"1", name:"bulbasaur", attack:"118", defense:"118", stamina:"90"});
-    pokemonList.push({number:"2", name:"ivysaur", attack:"151", defense:"151", stamina:"120"});
-    pokemonList.push({number:"3", name:"venusaur", attack:"198", defense:"198", stamina:"160"});
-    pokemonList.push({number:"4", name:"charmander", attack:"116", defense:"96", stamina:"78"});
-    pokemonList.push({number:"5", name:"charmeleon", attack:"158", defense:"129", stamina:"116"});
-    pokemonList.push({number:"6", name:"charizard", attack:"223", defense:"176", stamina:"156"});
-    pokemonList.push({number:"7", name:"squirtle", attack:"94", defense:"122", stamina:"88"});
-    pokemonList.push({number:"8", name:"wartortle", attack:"126", defense:"155", stamina:"118"});
-    pokemonList.push({number:"9", name:"blastoise", attack:"171", defense:"210", stamina:"158"});
-    pokemonList.push({number:"10", name:"caterpie", attack:"55", defense:"62", stamina:"90"});
-    pokemonList.push({number:"11", name:"metapod", attack:"45", defense:"94", stamina:"100"});
-    pokemonList.push({number:"12", name:"butterfree", attack:"167", defense:"151", stamina:"120"});
-    pokemonList.push({number:"13", name:"weedle", attack:"63", defense:"55", stamina:"80"});
-    pokemonList.push({number:"14", name:"kakuna", attack:"46", defense:"86", stamina:"90"});
-    pokemonList.push({number:"15", name:"beedrill", attack:"169", defense:"150", stamina:"130"});
-    pokemonList.push({number:"16", name:"pidgey", attack:"85", defense:"76", stamina:"80"});
-    pokemonList.push({number:"17", name:"pidgeotto", attack:"117", defense:"108", stamina:"126"});
-    pokemonList.push({number:"18", name:"pidgeot", attack:"166", defense:"157", stamina:"166"});
-    pokemonList.push({number:"19", name:"rattata", attack:"103", defense:"70", stamina:"60"});
-    pokemonList.push({number:"20", name:"raticate", attack:"161", defense:"144", stamina:"110"});
-}
+pokemonList.push({number:"1", name:"bulbasaur", type:"Grass / Poison", attack:"118", defense:"118", stamina:"90"});
+pokemonList.push({number:"2", name:"ivysaur", type:"Grass / Poison", attack:"151", defense:"151", stamina:"120"});
+pokemonList.push({number:"3", name:"venusaur", type:"Grass / Poison", attack:"198", defense:"198", stamina:"160"});
+pokemonList.push({number:"4", name:"charmander", type:"Fire", attack:"116", defense:"96", stamina:"78"});
+pokemonList.push({number:"5", name:"charmeleon", type:"Fire", attack:"158", defense:"129", stamina:"116"});
+pokemonList.push({number:"6", name:"charizard", type:"Fire / Flying", attack:"223", defense:"176", stamina:"156"});
+pokemonList.push({number:"7", name:"squirtle", type:"Water", attack:"94", defense:"122", stamina:"88"});
+pokemonList.push({number:"8", name:"wartortle", type:"Water", attack:"126", defense:"155", stamina:"118"});
+pokemonList.push({number:"9", name:"blastoise", type:"Water", attack:"171", defense:"210", stamina:"158"});
+pokemonList.push({number:"10", name:"caterpie", type:"Bug", attack:"55", defense:"62", stamina:"90"});
+pokemonList.push({number:"11", name:"metapod", type:"Bug", attack:"45", defense:"94", stamina:"100"});
+pokemonList.push({number:"12", name:"butterfree", type:"Bug / Flying", attack:"167", defense:"151", stamina:"120"});
+pokemonList.push({number:"13", name:"weedle", type:"Bug / Poison", attack:"63", defense:"55", stamina:"80"});
+pokemonList.push({number:"14", name:"kakuna", type:"Bug / Poison", attack:"46", defense:"86", stamina:"90"});
+pokemonList.push({number:"15", name:"beedrill", type:"Bug / Poison", attack:"169", defense:"150", stamina:"130"});
+pokemonList.push({number:"16", name:"pidgey", type:"Normal / Flying", attack:"85", defense:"76", stamina:"80"});
+pokemonList.push({number:"17", name:"pidgeotto", type:"Normal / Flying", attack:"117", defense:"108", stamina:"126"});
+pokemonList.push({number:"18", name:"pidgeot", type:"Normal / Flying", attack:"166", defense:"157", stamina:"166"});
+pokemonList.push({number:"19", name:"rattata", type:"Normal", attack:"103", defense:"70", stamina:"60"});
+pokemonList.push({number:"20", name:"raticate", type:"Normal", attack:"161", defense:"144", stamina:"110"});
 
 // this function checks to make sure the number search box only contains numbers
 function validateSearchNumber() {
@@ -65,6 +63,8 @@ function searchByNumber() {
         if (pokemonList[i].number == searchKey) {
             searchResults.unshift(
                 "Name: " + pokemonList[i].name + 
+                "\nNumber: " + pokemonList[i].number +
+                "\nType: " + pokemonList[i].type +
                 "\nAttack: " + pokemonList[i].attack +
                 "\nDefense: " + pokemonList[i].defense +
                 "\nStamina: " + pokemonList[i].stamina);
@@ -72,6 +72,8 @@ function searchByNumber() {
         else if (pokemonList[i].number.includes(searchKey)) {
             searchResults.push(
                 "Name: " + pokemonList[i].name + 
+                "\nNumber: " + pokemonList[i].number +
+                "\nType: " + pokemonList[i].type +
                 "\nAttack: " + pokemonList[i].attack +
                 "\nDefense: " + pokemonList[i].defense +
                 "\nStamina: " + pokemonList[i].stamina);
