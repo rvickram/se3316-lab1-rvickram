9d0443e92301c6e6042279ef1cbd4ac53978d1d7
Ryan Vickramasinghe <rvickram@uwo.ca>
2020-09-16 17:37:45 -0400

    implemented numberic search.

--- js/search.js
+++ js/search.js
@@ -1,25 +1,27 @@
 var pokemonList = []
 
-pokemon.push({number:"1", name:"bulbasaur", attack:"118", defense:"118", stamina:"90"});
-pokemon.push({number:"2", name:"ivysaur", attack:"151", defense:"151", stamina:"120"});
-pokemon.push({number:"3", name:"venusaur", attack:"198", defense:"198", stamina:"160"});
-pokemon.push({number:"4", name:"charmander", attack:"116", defense:"96", stamina:"78"});
-pokemon.push({number:"5", name:"charmeleon", attack:"158", defense:"129", stamina:"116"});
-pokemon.push({number:"6", name:"charizard", attack:"223", defense:"176", stamina:"156"});
-pokemon.push({number:"7", name:"squirtle", attack:"94", defense:"122", stamina:"88"});
-pokemon.push({number:"8", name:"wartortle", attack:"126", defense:"155", stamina:"118"});
-pokemon.push({number:"9", name:"blastoise", attack:"171", defense:"210", stamina:"158"});
-pokemon.push({number:"10", name:"caterpie", attack:"55", defense:"62", stamina:"90"});
-pokemon.push({number:"11", name:"metapod", attack:"45", defense:"94", stamina:"100"});
-pokemon.push({number:"12", name:"butterfree", attack:"167", defense:"151", stamina:"120"});
-pokemon.push({number:"13", name:"weedle", attack:"63", defense:"55", stamina:"80"});
-pokemon.push({number:"14", name:"kakuna", attack:"46", defense:"86", stamina:"90"});
-pokemon.push({number:"15", name:"beedrill", attack:"169", defense:"150", stamina:"130"});
-pokemon.push({number:"16", name:"pidgey", attack:"85", defense:"76", stamina:"80"});
-pokemon.push({number:"17", name:"pidgeotto", attack:"117", defense:"108", stamina:"126"});
-pokemon.push({number:"18", name:"pidgeot", attack:"166", defense:"157", stamina:"166"});
-pokemon.push({number:"19", name:"rattata", attack:"103", defense:"70", stamina:"60"});
-pokemon.push({number:"20", name:"raticate", attack:"161", defense:"144", stamina:"110"});
+function load() {
+    pokemonList.push({number:"1", name:"bulbasaur", attack:"118", defense:"118", stamina:"90"});
+    pokemonList.push({number:"2", name:"ivysaur", attack:"151", defense:"151", stamina:"120"});
+    pokemonList.push({number:"3", name:"venusaur", attack:"198", defense:"198", stamina:"160"});
+    pokemonList.push({number:"4", name:"charmander", attack:"116", defense:"96", stamina:"78"});
+    pokemonList.push({number:"5", name:"charmeleon", attack:"158", defense:"129", stamina:"116"});
+    pokemonList.push({number:"6", name:"charizard", attack:"223", defense:"176", stamina:"156"});
+    pokemonList.push({number:"7", name:"squirtle", attack:"94", defense:"122", stamina:"88"});
+    pokemonList.push({number:"8", name:"wartortle", attack:"126", defense:"155", stamina:"118"});
+    pokemonList.push({number:"9", name:"blastoise", attack:"171", defense:"210", stamina:"158"});
+    pokemonList.push({number:"10", name:"caterpie", attack:"55", defense:"62", stamina:"90"});
+    pokemonList.push({number:"11", name:"metapod", attack:"45", defense:"94", stamina:"100"});
+    pokemonList.push({number:"12", name:"butterfree", attack:"167", defense:"151", stamina:"120"});
+    pokemonList.push({number:"13", name:"weedle", attack:"63", defense:"55", stamina:"80"});
+    pokemonList.push({number:"14", name:"kakuna", attack:"46", defense:"86", stamina:"90"});
+    pokemonList.push({number:"15", name:"beedrill", attack:"169", defense:"150", stamina:"130"});
+    pokemonList.push({number:"16", name:"pidgey", attack:"85", defense:"76", stamina:"80"});
+    pokemonList.push({number:"17", name:"pidgeotto", attack:"117", defense:"108", stamina:"126"});
+    pokemonList.push({number:"18", name:"pidgeot", attack:"166", defense:"157", stamina:"166"});
+    pokemonList.push({number:"19", name:"rattata", attack:"103", defense:"70", stamina:"60"});
+    pokemonList.push({number:"20", name:"raticate", attack:"161", defense:"144", stamina:"110"});
+}
 
 // this function checks to make sure the number search box only contains numbers
 function validateSearchNumber() {
@@ -37,11 +39,11 @@ function validateSearchNumber() {
     }
 }
 
-function checkSearchText() {
+function validateSearchText() {
     text = document.getElementById("searchtext").value;
     
     // check that input is maximum 20 characters long
-    if (text.length >= 20) {
+    if (text.length > 20) {
         alert("Your search may only be 20 characters long!");
     }
 
@@ -56,16 +58,49 @@ function checkSearchText() {
 
 // this function executes the search by numbers
 function searchByNumber() {
-    alert("Search for pokemon!");
-    for(i = 0; i < 20; i++) {
+    var searchKey = document.getElementById("searchnumber").value;
+    var searchResults = [];
 
+    for(i = 0; i < 20; i++) {
+        if (pokemonList[i].number == searchKey) {
+            searchResults.unshift(
+                "Name: " + pokemonList[i].name + 
+                "\nAttack: " + pokemonList[i].attack +
+                "\nDefense: " + pokemonList[i].defense +
+                "\nStamina: " + pokemonList[i].stamina);
+        }
+        else if (pokemonList[i].number.includes(searchKey)) {
+            searchResults.push(
+                "Name: " + pokemonList[i].name + 
+                "\nAttack: " + pokemonList[i].attack +
+                "\nDefense: " + pokemonList[i].defense +
+                "\nStamina: " + pokemonList[i].stamina);
+        }
+
+        if (searchResults.length == 5) break;
     }
+
+    var message = "Showing first 5 results (Click anywhere to close):";
+    for(i = 0; i < searchResults.length; i++) {
+        message += "\n\n" + searchResults[i];
+    }
+    showMessage(message);
 }
 
 // this function executes the search by Text
 function searchByText() {
     alert("Search for pokemon!");
     for(i = 0; i < 20; i++) {
-
+        
     }
+}
+
+// this function was created to display long search results in a closeable way:
+function showMessage(message) {
+    document.getElementById("overlay_text").innerText = message;
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeMessage() {
+    document.getElementById("overlay").style.display = "none";
 }
